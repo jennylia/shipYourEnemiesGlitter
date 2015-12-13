ab6b036024bd1921557ec339447bf63ef912f29f
Jenny Lian <jenny@fingerfoodstudios.com>
2015-12-12 21:49:22 -0800

    try to make a separate page for firebase

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
-var app=express();
+var app = express();
+var Firebase = require("firebase");
+
 /*var smtpTransport = nodemailer.createTransport({
-	service: "Gmail",
-	    auth: {
-		    user: "happy123demo@gmail.com",
-   		 	}
-});
-*/
+ service: "Gmail",
+ auth: {
+ user: "happy123demo@gmail.com",
+ }
+ });
+ */
 
 var smtpTransport = nodemailer.createTransport({
-	host: "mailtrap.io",
-    	port: 2525,
-    	auth: {
-		user: "52885226139a9f766",
-    		pass: "3f12b8b855a0a6"
-	}
+    host: "mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "52885226139a9f766",
+        pass: "3f12b8b855a0a6"
+    }
 });
 
-app.get('/', function(req,res){
-	res.sendfile('index.html');
+app.get('/contacts', function(req,res){
+    res.sendFile('contacts.html');
 });
 
-app.get('/send', function(req,res){
-	console.log("sending email");
-	console.log(req.query);
-	console.log("req query end");
-	var mailOptions= {
-		from: 'Jenny',
-		to: req.query.to,
-		subject: req.query.subject,
-		text: req.query.text,
-		html:'<b>Hello World</b>'
-	}
 
-	console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(err, res){
-		if(err){
-			console.log(err);
-		}else{
-			console.log(res);
-		}
-	});
+app.get('/', function(req, res)
+{
+    res.sendFile('index.html');
 });
 
-app.listen(3000, function(){
-	console.log(3000);
+app.get('/send', function(req, res)
+{
+    console.log("sending email");
+    console.log(req.query);
+    console.log("req query end");
+    var mailOptions = {
+        from: 'Jenny',
+        to: req.query.to,
+        subject: req.query.subject,
+        text: req.query.text,
+        html: '<b>Hello World</b>'
+    }
+
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(err, res)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+            console.log(res);
+    });
+});
+
+app.listen(3000, function()
+{
+    console.log(3000);
 });
